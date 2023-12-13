ea8f9bf934733d2e87354eb93b7268e1c266ef4f
Gabriel <gabriel.silva@ecine.com.br>
2023-12-13 11:09:02 -0300

    Fix Lavalink redirect uri release

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-curl "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.10/Lavalink.jar" -o Lavalink.jar
+curl -L "https://github.com/lavalink-devs/Lavalink/releases/download/3.7.10/Lavalink.jar" -o Lavalink.jar
 
 java -jar Lavalink.jar
